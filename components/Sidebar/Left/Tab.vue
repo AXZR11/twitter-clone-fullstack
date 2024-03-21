f92638c29793b218418851049a7f7cf4157c0801
AXZR11 <sasha.boy16@gmail.com>
2024-03-22 00:02:30 +0300

    implemented animated logo

---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,13 @@
+<template>
+    <nuxt-link to="#">
+        <div>
+            <slot name="icon"></slot>
+        </div>
+
+        <div>
+            <slot name="name"></slot>
+        </div>
+    </nuxt-link>
+</template>
+<script setup>
+</script>
